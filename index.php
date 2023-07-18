9efcc53620c6b8c096f1608b788a2b533eb27708
unknown <901.anshika@gmail.com>
2023-07-18 15:11:34 +0530

    Initial commit

---
+++ index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Todo List Application</title>
+</head>
+<body>
+  <main>
+<div class="conatiner" pt-5>
+        <div class="card">
+            <div class="card-header">
+                <p>Todo List</p>
+            </div>
+            <div class="card-body">
+                <form action="">
+                    <div>
+                        <input type="text" class="form-control"name="item" placeholder="Add a Todo Item">
+
+                    </div>
+                    <input type="submit" class="btn btn-dark" name="add" value="Add Item">
+                </form>
+            </div>
+        </div>
+    </div> 
+</main>
+
+
+
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/29fNfEXIDAYTUJMAEBASjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.3/jquery.min.js"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/cesiumjs/1.78/Build/Cesium/Cesium.js"></script> 
+ <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script> -->
+
+
+</body>
+</html>
